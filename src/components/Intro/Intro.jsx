db8b55e071beb513560421b3c679a4d082629c5c
Ben <duychien152003@gmail.com>
2024-12-24 18:42:23 +0700

    ci: update CI/CD pipeline configuration
    Updated the CI/CD pipeline to include Docker image build and push steps, and deployment to Heroku.

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,23 +1,23 @@
-import { motion } from "framer-motion";
-import React, { useContext } from "react";
-import { Link } from "react-scroll";
-import { themeContext } from "../../Context";
-import Vector1 from "../../img/Vector1.png";
-import Vector2 from "../../img/Vector2.png";
-import boy from "../../img/boy.png";
-import crown from "../../img/crown.png";
-import Github from "../../img/github.png";
-import glassesimoji from "../../img/glassesimoji.png";
-import Instagram from "../../img/instagram.png";
-import LinkedIn from "../../img/linkedin.png";
-import thumbup from "../../img/thumbup.png";
-import FloatinDiv from "../FloatingDiv/FloatingDiv";
-import urls from "../constant/url";
-import "./Intro.css";
+import { motion } from 'framer-motion';
+import React, { useContext } from 'react';
+import { Link } from 'react-scroll';
+import { themeContext } from '../../Context';
+import Vector1 from '../../img/Vector1.png';
+import Vector2 from '../../img/Vector2.png';
+import boy from '../../img/boy.png';
+import crown from '../../img/crown.png';
+import Github from '../../img/github.png';
+import glassesimoji from '../../img/glassesimoji.png';
+import Instagram from '../../img/instagram.png';
+import LinkedIn from '../../img/linkedin.png';
+import thumbup from '../../img/thumbup.png';
+import FloatinDiv from '../FloatingDiv/FloatingDiv';
+import urls from '../constant/url';
+import './Intro.css';
 
 const Intro = () => {
   // Transition
-  const transition = { duration: 2, type: "spring" };
+  const transition = { duration: 2, type: 'spring' };
 
   // context
   const theme = useContext(themeContext);
@@ -29,11 +29,11 @@ const Intro = () => {
       <div className="i-left">
         <div className="i-name">
           {/* yahan change hy darkmode ka */}
-          <span style={{ color: darkMode ? "white" : "" }}>Hi! I Am</span>
+          <span style={{ color: darkMode ? 'white' : '' }}>Hi! I Am</span>
           <span>Phạm Duy Chiến </span>
           <span>
-            DevOps Engineer with extensive experience in web design and development, delivering high-quality
-            work
+            DevOps Engineer with extensive experience in web design and
+            development, delivering high-quality work
           </span>
         </div>
         <Link to="contact" smooth={true} spy={true}>
@@ -59,16 +59,16 @@ const Intro = () => {
         <img src={boy} alt="" />
         {/* animation */}
         <motion.img
-          initial={{ left: "-36%" }}
-          whileInView={{ left: "-24%" }}
+          initial={{ left: '-36%' }}
+          whileInView={{ left: '-24%' }}
           transition={transition}
           src={glassesimoji}
           alt=""
         />
 
         <motion.div
-          initial={{ top: "-4%", left: "74%" }}
-          whileInView={{ left: "68%" }}
+          initial={{ top: '-4%', left: '74%' }}
+          whileInView={{ left: '68%' }}
           transition={transition}
           className="floating-div"
         >
@@ -77,8 +77,8 @@ const Intro = () => {
 
         {/* animation */}
         <motion.div
-          initial={{ left: "9rem", top: "18rem" }}
-          whileInView={{ left: "0rem" }}
+          initial={{ left: '9rem', top: '18rem' }}
+          whileInView={{ left: '0rem' }}
           transition={transition}
           className="floating-div"
         >
@@ -86,15 +86,15 @@ const Intro = () => {
           <FloatinDiv img={thumbup} text1="Best Design" text2="Award" />
         </motion.div>
 
-        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
+        <div className="blur" style={{ background: 'rgb(238 210 255)' }}></div>
         <div
           className="blur"
           style={{
-            background: "#C1F5FF",
-            top: "17rem",
-            width: "21rem",
-            height: "11rem",
-            left: "-9rem",
+            background: '#C1F5FF',
+            top: '17rem',
+            width: '21rem',
+            height: '11rem',
+            left: '-9rem',
           }}
         ></div>
       </div>
